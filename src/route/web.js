319511f58f144c4a416b64ef6b47fa2a770caa34
Vuonggithubb <fanvuong99@gmail.com>
2024-06-06 13:10:46 +0700

    2

--- src/route/web.js
+++ src/route/web.js
@@ -20,6 +20,9 @@ let initWebRoutes = (app) => {
 
     router.post('/api/login', userController.handleLoging);
     router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/creat-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser);
 
     return app.use("/", router);
 }
